3d964c5e730931f5cade041ca19a7177303bbdc5
Mattias Didriksson <mattias.didriksson@gmail.com>
2020-12-03 21:49:10 +0100

    Känns som att jag missar något smart här...

--- advent-of-code-clojure/src/advent_of_code/day_03.clj
+++ advent-of-code-clojure/src/advent_of_code/day_03.clj
@@ -1,11 +1,28 @@
-(ns advent-of-code.day-03)
+(ns advent-of-code.day-03
+  (:require [clojure.string :as str]))
+
+
+(defn getForPosition [input width x y]
+  (if (= \# (get input (+ (rem x width) (* y width))))
+    1
+    0))
+
+(defn doForSlope 
+  [input slope]
+  (let [width (count (first (str/split input #"\r\n")))]
+    (loop [x 0 y 0 trees 0]
+      (if (>= y (/ (count (str/replace input #"\r\n" "")) width))
+        trees
+        (recur (+ x (first slope)) (+ y (last slope)) (+ (getForPosition (str/replace input #"\r\n" "") width x y) trees))))))
 
 (defn part-1
   "Day 03 Part 1"
   [input]
-  input)
+  (doForSlope input '(3 1)))
+
+(def slopes '( (1 1) (3 1) (5 1) (7 1) (1 2)))
 
 (defn part-2
   "Day 03 Part 2"
   [input]
-  input)
+  (reduce * (map (fn [slope] (doForSlope input slope)) slopes)))
