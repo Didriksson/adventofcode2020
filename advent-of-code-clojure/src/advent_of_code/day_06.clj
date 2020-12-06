3f30cf91490e428ca20cdaa4b3097fddbd9819dd
Mattias Didriksson <mattias.didriksson@gmail.com>
2020-12-06 15:38:14 +0100

    Dag 6

--- advent-of-code-clojure/src/advent_of_code/day_06.clj
+++ advent-of-code-clojure/src/advent_of_code/day_06.clj
@@ -1,11 +1,24 @@
-(ns advent-of-code.day-06)
+(ns advent-of-code.day-06
+  (:require [clojure.string :as str]))
+
+
+(defn group-until-blank
+  [input]
+  (filter #(not= '("") %) (partition-by str/blank? (str/split-lines input))))
+
+(defn occurances-for-group [group]
+  (apply merge-with + (map frequencies  group)))
 
 (defn part-1
   "Day 06 Part 1"
   [input]
-  input)
+  (reduce + (map count (map occurances-for-group (group-until-blank input)))))
+
+(defn allAnswered [input]
+  (filter #(= (second %) (count input)) (occurances-for-group input)))
+
 
 (defn part-2
   "Day 06 Part 2"
   [input]
-  input)
+  (reduce + (map count (map allAnswered (group-until-blank input)))))
