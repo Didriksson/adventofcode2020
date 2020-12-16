4f10a5e0c0f990bdf2e089bf979fc50072f7fdd1
Mattias Didriksson <mattias.didriksson@lm.se>
2020-12-16 16:39:26 +0100

    Dag 9 p1.

--- advent-of-code-clojure/src/advent_of_code/day_07.clj
+++ advent-of-code-clojure/src/advent_of_code/day_07.clj
@@ -1,11 +1,22 @@
-(ns advent-of-code.day-07)
+(ns advent-of-code.day-07
+  (:require [clojure.string :as str])) 
+
+
+(defn to-rules 
+[line]
+  (let [bagWithContent (map str/trim (str/split line #"contain"))]
+    (assoc {} (first bagWithContent) (rest bagWithContent))
+  ) 
+)
 
 (defn part-1
   "Day 07 Part 1"
   [input]
-  input)
+  (filter (fn [x] (str/includes? x "shiny gold")) (mapcat vals (map to-rules (str/split-lines input)))))
 
 (defn part-2
   "Day 07 Part 2"
   [input]
   input)
+
+
--- advent-of-code-clojure/src/advent_of_code/day_09.clj
+++ advent-of-code-clojure/src/advent_of_code/day_09.clj
@@ -1,11 +1,30 @@
-(ns advent-of-code.day-09)
+(ns advent-of-code.day-09
+  (:require [clojure.math.combinatorics :as combo]))
+
+(import '(java.io BufferedReader StringReader))
+
+
+(defn inputAsInt [input] (map #(Long/parseLong %) (line-seq (BufferedReader. (StringReader. input)))))
+
+
+(defn valid? [number preamble]
+  (some #(= number (+ (first %) (last %))) (combo/combinations preamble 2)))
+
+(defn first-invalid [steps input]
+  (loop [listTocheck input]
+    (if (valid? (first (drop steps listTocheck)) (take steps listTocheck))
+        (recur (drop 1 listTocheck))
+        (first (drop steps listTocheck))  
+      )
+    )
+  )
 
 (defn part-1
   "Day 09 Part 1"
   [input]
-  input)
+  (first-invalid 25 (inputAsInt input)))
 
 (defn part-2
   "Day 09 Part 2"
   [input]
-  input)
+  (inputAsInt input))
