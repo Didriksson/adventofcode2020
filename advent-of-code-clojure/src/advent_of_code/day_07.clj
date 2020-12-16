280587c085d564c12ff4624b37230fad159fe794
Mattias Didriksson <mattias.didriksson@gmail.com>
2020-12-16 20:59:24 +0100

    Lite fortsatt groll med 7an och 8an.

--- advent-of-code-clojure/src/advent_of_code/day_07.clj
+++ advent-of-code-clojure/src/advent_of_code/day_07.clj
@@ -1,11 +1,51 @@
-(ns advent-of-code.day-07)
+(ns advent-of-code.day-07
+  (:require [clojure.string :as str]))
+
+
+(defn parse-one-row [row]
+  (let [item (map str/trim (str/split (str/trim (str/replace (str/replace row #"\." "") #"bags|bag" "")) #"contain"))]
+    (mapcat #(str/split % #" , ") item)))
+
+
+(defn find-containing-bag [bag rule]
+  (when (some #(= % bag) rule)
+    (first rule)))
+
+(defn find-all-bags-containing [bag rules]
+
+  (loop [bagsToFind (list bag) foundbags '()]
+    (if (empty? bagsToFind)
+      foundbags
+      (let [newBags (remove nil? (map #(find-containing-bag (first bagsToFind) %) rules))]
+        (recur (remove (set foundbags) (distinct (concat (rest bagsToFind) newBags))) (distinct (conj foundbags (first bagsToFind))))))))
 
 (defn part-1
   "Day 07 Part 1"
   [input]
-  input)
+  (let [bags  (distinct (find-all-bags-containing "shiny gold" (map parse-one-row (str/split-lines (str/replace input  #"\d\s" "")))))]
+    (-
+     (count bags)
+     1)))
+
+(defn parse-containing-bags [content]
+  (map #(list (Integer/parseInt (first %)) (keyword (str/join "-" (rest %)))) (partition 3 (str/split (str/replace content #", " "") #" "))))
+(defn parse-row-with-numbers [row]
+  (let [[x xs] (str/split (str/replace (str/replace row #"\." "") #"bags|bag" "") #" contain ")]
+    (assoc {} (keyword (str/replace (str/trim x) #" " "-")) (parse-containing-bags xs))))
+
+(defn find-all-containing [input bag result]
+  (let [foundBags (get input (keyword (second bag)))]
+    (if (empty? foundBags)
+      result
+      (merge (first foundBags) (map #(find-all-containing input % (merge result foundBags)) foundBags)))
+    )
+  )
+
+
 
 (defn part-2
   "Day 07 Part 2"
   [input]
-  input)
+  (let [bag-map (into {} (map parse-row-with-numbers (str/split-lines input)))]
+    (find-all-containing bag-map (list 1 (keyword "shiny-gold")) '()))
+  )
--- advent-of-code-clojure/src/advent_of_code/day_08.clj
+++ advent-of-code-clojure/src/advent_of_code/day_08.clj
@@ -1,11 +1,48 @@
-(ns advent-of-code.day-08)
+(ns advent-of-code.day-08
+  (:require [clojure.string :as str]))
+
+
+(defn parseInput [input]
+  (map #(vector (first  %) (Integer/parseInt (last %))) (map #(str/split % #" ") (str/split-lines input))))
+
+(defn apply-command [commands pointer acc]
+  (let [[cmd val] (nth commands pointer)]
+    (case cmd
+      "acc" [(inc pointer) (+ val acc)]
+      "jmp" [(+ val pointer) acc]
+      "nop" [(inc pointer) acc])))
+
+(defn run-til-loop-or-terminated [input]
+  (loop [pointer 0 acc 0 cmdHistory []]
+    (if (some #{pointer} cmdHistory)
+      [:looped acc]
+      (if (>= pointer (count input))
+        [:terminated acc]
+        (let [[po ac] (apply-command input pointer acc)]
+          (recur po ac (conj cmdHistory pointer)))))))
+
+(defn replaceCmd [input index]
+  
+  (let [item (nth input index)]
+    (if (= (first item) "jmp")
+      (assoc (into [] input) index (vector "nop" (last item)))
+      (assoc (into [] input) index (vector "jmp" (last item))))))
+
+(defn replace-and-run-til-terminated [input]
+  (loop [cmdToChangeIndexes (keep-indexed #(when (or (= (first %2) "nop") (= (first %2) "jmp")) %) input)]
+    (let [newInput (replaceCmd input (last cmdToChangeIndexes))]
+      (let [[exitcode acc] (run-til-loop-or-terminated newInput)]
+        (println exitcode)
+        (case exitcode
+          :terminated [exitcode acc]
+          :looped (recur (drop-last cmdToChangeIndexes)))))))
 
 (defn part-1
   "Day 08 Part 1"
   [input]
-  input)
+  (run-til-loop-or-terminated (parseInput input)))
 
 (defn part-2
   "Day 08 Part 2"
   [input]
-  input)
+  (replace-and-run-til-terminated (parseInput input)))
